d386052af3928d6e3e55ba04e5ab0cb6bc68a606
Elivelton Moura <eliveltonmoura112@gmail.com>
2023-11-24 15:35:22 -0300

    Atuação da tarde

--- Projeto_atv3/Main.java
+++ Projeto_atv3/Main.java
@@ -1,4 +1,4 @@
-package Projeto;
+package Projeto_atv3;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -84,7 +84,7 @@ class Produto {
 
 }
 
-class Transacao {
+/*class Transacao {
     private int id;
     private TipoTransacao tipo;
     private int idProduto;
@@ -96,7 +96,7 @@ class Transacao {
 
 enum TipoTransacao {
     ENTRADA, SAIDA
-}
+}*/
 
 class Venda {
     private int id;
@@ -146,12 +146,12 @@ class Venda {
         this.data = data;
     }
 
-     public Venda(int id, int idProduto, int quantidade, double valorTotal, Date data) {
+     public Venda(int id, int idProduto, int quantidade, double valorTotal, String string) {
         this.id = id;
         this.idProduto = idProduto;
         this.quantidade = quantidade;
         this.valorTotal = valorTotal;
-        this.data = data;
+        this.data = string;
     }
 
 }
@@ -161,7 +161,7 @@ class Compra {
     private int idProduto;
     private int quantidade;
     private double valorTotal;
-    private Date data;
+    private String data;
 
     // Construtor, getters e setters
     public int getId() {
@@ -200,11 +200,11 @@ class Compra {
         return data;
     }
 
-    public void setData(Date data) {
+    public void setData(String data) {
         this.data = data;
     }
 
-     public Compra(int id, int idProduto, int quantidade, double valorTotal, Date data) {
+     public Compra(int id, int idProduto, int quantidade, double valorTotal, String data) {
         this.id = id;
         this.idProduto = idProduto;
         this.quantidade = quantidade;
@@ -216,36 +216,84 @@ class Compra {
 
 class SistemaEstoque {
     private List<Produto> produtos;
-    private List<Transacao> transacoes;
+    //private List<Transacao> transacoes;
     private List<Venda> vendas;
     private List<Compra> compras;
 
     public SistemaEstoque() {
         this.produtos = new ArrayList<>();
-        this.transacoes = new ArrayList<>();
+        //this.transacoes = new ArrayList<>();
         this.vendas = new ArrayList<>();
         this.compras = new ArrayList<>();
     }
 
     public void adicionarProduto(Produto produto) {
-        // Lógica para adicionar um produto
+          if (!produtos.contains(produto)) {
+            // Adicionar o produto à lista de produtos
+            produtos.add(produto);
+            System.out.println("Produto adicionado com sucesso!");
+        } else {
+            System.out.println("Produto já existe no estoque.");
+        }
     }
 
     public void registrarVenda(Venda venda) {
-        // Lógica para registrar uma venda
+       Produto produto = buscarProdutoPorId(venda.getIdProduto());
+
+        if (produto != null && produto.getQuantidadeEstoque() >= venda.getQuantidade()) {
+            // Atualizar o estoque
+            produto.setQuantidadeEstoque(produto.getQuantidadeEstoque() - venda.getQuantidade());
+
+            // Registrar a venda
+            vendas.add(venda);
+
+            System.out.println("Venda registrada com sucesso!");
+        } else {
+            System.out.println("Produto não encontrado no estoque ou quantidade insuficiente.");
+
+        }
+
+        private Produto buscarProdutoPorId(int idProduto) {
+        for (Produto produto : produtos) {
+            if (produto.getId() == idProduto) {
+                return produto;
+            }
+        }
+        return null;
     }
+   
 
     public void registrarCompra(Compra compra) {
-        // Lógica para registrar uma compra
+        Produto produto = buscarProdutoPorId(compra.getIdProduto());
+
+        if (produto != null) {
+            // Atualizar o estoque
+            produto.setQuantidadeEstoque(produto.getQuantidadeEstoque() + compra.getQuantidade());
+
+            // Registrar a compra
+            compras.add(compra);
+
+            System.out.println("Compra registrada com sucesso!");
+        } else {
+            System.out.println("Produto não encontrado no estoque.");
+        }
     }
 
     public void exibirRelatorioEstoque() {
-        // Lógica para exibir o relatório de estoque
+       System.out.println("=== Relatório de Estoque ===");
+
+        for (Produto produto : produtos) {
+            System.out.println("ID: " + produto.getId());
+            System.out.println("Nome: " + produto.getNome());
+            System.out.println("Categoria: " + produto.getCategoria());
+            System.out.println("Marca: " + produto.getMarca());
+            System.out.println("Quantidade em Estoque: " + produto.getQuantidadeEstoque());
+            System.out.println("===========================");
+        }
     }
 
-    // Outras funcionalidades...
-
-    // Métodos getters e setters
+    
+  
 }
 
 public class Main {
@@ -253,11 +301,11 @@ public class Main {
         SistemaEstoque sistema = new SistemaEstoque();
 
         // Exemplo: Adicionar um produto
-        Produto produto = new Produto(/* dados do produto */);
+        Produto produto = new Produto(25,"fugão","cosinha","consul","Magalu",2.000,4,3);
         sistema.adicionarProduto(produto);
 
         // Exemplo: Registrar uma venda
-        Venda venda = new Venda(/* dados da venda */);
+        Venda venda = new Venda(33,25,1,3.000,"20/11/2023");
         sistema.registrarVenda(venda);
 
         // Exemplo: Registrar uma compra
